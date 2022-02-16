9b78d9b45bdb2fc4080d8d50cbbe6127afe9326c
unknown <aylentorres27@gmail.com>
2022-02-16 20:22:07 -0300

    Añadiendo nuevos cambios

--- Basic Calculator/BasicCalculator.py
+++ Basic Calculator/BasicCalculator.py
@@ -3,4 +3,12 @@ num2 = int(input('Enter second number: '))
 op = input('Enter Operator: ')
 
 if op == '+':
-    print(num1 + num2)
+    print('The addition is', num1 + num2)
+elif op == '-':
+    print('The substraction is', num1 - num2)
+elif op == '*':
+    print('The multiplication is', num1*num2)
+elif op == '/':
+    print('The division is', abs(num1/num2))
+else:
+    print('Invalid operator!')
