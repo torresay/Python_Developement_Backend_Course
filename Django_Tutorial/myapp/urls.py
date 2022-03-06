a7a095be552bc67363c6d61d81191300894ecbaa
unknown <aylentorres27@gmail.com>
2022-03-06 10:58:19 -0300

    Añadiendo nuevos cambiosÂ

--- Django_Tutorial/myapp/urls.py
+++ Django_Tutorial/myapp/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name ='index')
+    path('', views.index, name ='index'),
+    path('counter', views.counter, name='counter')
 ]
--- Django_Tutorial/myapp/views.py
+++ Django_Tutorial/myapp/views.py
@@ -1,7 +1,14 @@
-import imp
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    features = Feature.objects.all()
+
+    return render(request, 'index.html', {'features': features})
+
+def counter(request):
+    text = request.POST['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
